ffbdb15cd791bf57402ddbe20e9e4e84f78b45e7
wangzuojun <wangzuojun@meituan.com>
2019-12-16 22:33:05 +0800

    FirstMissingPositive init

---
+++ src/code/code_0041/FirstMissingPositive.java
@@ -0,0 +1,14 @@
+package code.code_0041;
+
+public class FirstMissingPositive {
+
+    /*
+    * 找到数组中 丢失的第一个正数
+    * 难点在于  1 常量空间 （考虑原地算法） 2 O(N)时间复杂度
+    * 如果不考虑常量空间复杂度  直接 用数组下表关联数组值 即可
+    * new 一个新数组缓存值 再遍历一遍看哪个位置没被set进去 就是丢失的第一个正数
+    * */
+    public int firstMissingPositive(int[] nums) {
+
+    }
+}
