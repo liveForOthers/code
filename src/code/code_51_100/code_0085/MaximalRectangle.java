cb947e1b3c3a9015a0f63a0e8d9c99be31b10304
wangzuojun <wangzuojun@meituan.com>
2020-01-03 17:46:31 +0800

    MaximalRectangle

---
+++ src/code/code_51_100/code_0085/MaximalRectangle.java
@@ -0,0 +1,33 @@
+package code.code_51_100.code_0085;
+
+public class MaximalRectangle {
+
+    /*
+    * Given a 2D binary matrix filled with 0's and 1's,
+    * find the largest rectangle containing only 1's and return its area.
+    *
+    * Example:
+    *
+    * Input:
+    * [["1","0","1","0","0"],
+    *  ["1","0","1","1","1"],
+    *  ["1","1","1","1","1"],
+    *  ["1","0","0","1","0"]]
+    *
+    * Output: 6
+    *
+    * 目标： 找到二维矩阵中 字符'1' 组成的最大矩形面积
+    *
+    * 算法：
+    * 维护三个变量
+    * 1 left   列左下标
+    * 2 right  列右下标
+    * 3 height 矩阵高度
+    *
+    * TODO 优点难  之后再思考下  先记下todo了
+    *
+    * */
+    public int maximalRectangle(char[][] matrix) {
+        return -1;
+    }
+}
