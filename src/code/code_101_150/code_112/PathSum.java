5f020672a8d638e3d64f64f8e1eb784ddeea5245
wangzuojun <wangzuojun@meituan.com>
2020-01-08 18:35:37 +0800

    PathSum

--- src/code/code_101_150/code_112/PathSum.java
+++ src/code/code_101_150/code_112/PathSum.java
@@ -1,6 +1,10 @@
 package code.code_101_150.code_112;
 
 import code.common.TreeNode;
+import javafx.util.Pair;
+
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class PathSum {
 
@@ -28,7 +32,7 @@ public class PathSum {
      * 算法：
      * 自底向上计算出当前节点为根节点的左右path的路径和 分为左path 以及 右path两个
      *
-     * TODO:  学习官方解法中的 递归与迭代方法 https://leetcode-cn.com/problems/path-sum/solution/lu-jing-zong-he-by-leetcode/
+     * 学习:  学习官方解法中的 递归与迭代方法 https://leetcode-cn.com/problems/path-sum/solution/lu-jing-zong-he-by-leetcode/
      * 并实现迭代递归方法
      *
      * */
@@ -67,8 +71,50 @@ public class PathSum {
         root.right.right = new TreeNode(4);
         root.right.right.right = new TreeNode(1);
 
-        boolean b = hasPathSum(root, 22);
+        boolean b = hasPathSum3(root, 22);
         System.out.println(b);
 
+    }
+
+    public static boolean hasPathSum2(TreeNode root, int sum) {
+        // 根节点为null 返回false
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        sum = sum - root.val;
+        return hasPathSum2(root.left, sum) || hasPathSum2(root.right, sum);
+    }
+
+    /*
+     * 使用迭代 栈模拟dfs 进行实现
+     *
+     * */
+    public static boolean hasPathSum3(TreeNode root, int sum) {
+        // 根节点为null 返回false
+        if (root == null) {
+            return false;
+        }
+        Deque<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        stack.push(new Pair<TreeNode, Integer>(root, sum));
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> curPair = stack.pop();
+            if (curPair.getKey().left == null && curPair.getKey().right == null) {
+                if (curPair.getValue() == curPair.getKey().val) {
+                    return true;
+                }
+                continue;
+            }
+            int leftValue = curPair.getValue() - curPair.getKey().val;
+            if (curPair.getKey().left != null) {
+                stack.push(new Pair<TreeNode, Integer>(curPair.getKey().left, leftValue));
+            }
+            if (curPair.getKey().right != null) {
+                stack.push(new Pair<TreeNode, Integer>(curPair.getKey().right, leftValue));
+            }
+        }
+        return false;
     }
 }
