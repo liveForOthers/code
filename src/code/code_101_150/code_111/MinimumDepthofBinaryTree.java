5bc51207eb7cc7f7bf6bb1f2c1811d981a1c5679
wangzuojun <wangzuojun@meituan.com>
2020-01-08 12:42:48 +0800

    MinimumDepthofBinaryTree

--- src/code/code_101_150/code_111/MinimumDepthofBinaryTree.java
+++ src/code/code_101_150/code_111/MinimumDepthofBinaryTree.java
@@ -1,6 +1,10 @@
 package code.code_101_150.code_111;
 
 import code.common.TreeNode;
+import javafx.util.Pair;
+
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class MinimumDepthofBinaryTree {
 
@@ -38,10 +42,10 @@ public class MinimumDepthofBinaryTree {
         if (root == null) {
             return 0;
         }
-        if (root.left==null && root.right==null){
+        if (root.left == null && root.right == null) {
             return 1;
         }
-        if (root.left==null || root.right==null){
+        if (root.left == null || root.right == null) {
             int leftDepth = minDepth(root.left);
             int rightDepth = minDepth(root.right);
             return Math.max(leftDepth, rightDepth) + 1;
@@ -50,5 +54,44 @@ public class MinimumDepthofBinaryTree {
         int rightDepth = minDepth(root.right);
         return Math.min(leftDepth, rightDepth) + 1;
     }
+
+    /*
+     * 使用迭代进行实现
+     * 本质是 使用栈模拟dfs
+     *          队列模拟 bfs
+     * */
+    public static int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Pair<TreeNode, Integer> pair = new Pair<TreeNode, Integer>(root, 1);
+        Deque<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.offer(pair);
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Integer> curPair = queue.pop();
+            if (curPair.getKey().left == null && curPair.getKey().right == null) {
+                return curPair.getValue();
+            }
+            if (curPair.getKey().left != null) {
+                queue.offer(new Pair<>(curPair.getKey().left, curPair.getValue() + 1));
+            }
+            if (curPair.getKey().right != null) {
+                queue.offer(new Pair<>(curPair.getKey().right, curPair.getValue() + 1));
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+
+        minDepth2(root);
+    }
+
 
 }
