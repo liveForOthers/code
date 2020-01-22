c35a1c90e7c450c25225b6aac4d4e1069d125649
wangzuojun <wangzuojun@meituan.com>
2020-01-22 20:00:41 +0800

    MaximumGap

---
+++ src/code/code_151_200/code_164/MaximumGap.java
@@ -0,0 +1,35 @@
+package code.code_151_200.code_164;
+
+public class MaximumGap {
+    /*
+    * Given an unsorted array,
+    * find the maximum difference between the successive elements in its sorted form.
+    * 找到连续形式相连的两个元素间的最大差值，要求线性时间复杂度实现
+    *
+    * Return 0 if the array contains less than 2 elements.
+    *
+    * Example 1:
+    *
+    * Input: [3,6,9,1]
+    * Output: 3
+    * Explanation: The sorted form of the array is [1,3,6,9],
+    * either(3,6) or (6,9) has the maximum difference 3.
+    *
+    * Example 2:
+    * Input: [10]
+    * Output: 0
+    *
+    * Explanation: The array contains less than 2 elements, therefore return 0.
+    *
+    * Note:
+    * You may assume all elements in the array are non-negative integers
+    * and fit in the 32-bit signed integer range.
+    * Try to solve it in linear time/space.
+    *
+    * 本题考察O(N) 时间复杂度的排序算法  主要是桶排序算法、基数排序算法
+    * TODO 学习：线性时间复杂度排序算法：桶排序算法、基数排序算法  https://leetcode-cn.com/problems/maximum-gap/solution/zui-da-jian-ju-by-leetcode/
+    * */
+    public int maximumGap(int[] nums) {
+        throw new IllegalArgumentException("待完成TODO");
+    }
+}
