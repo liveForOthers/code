3c015f38a1e1ce4d1f451b381b77463f12efdf70
wangzuojun <wangzuojun@meituan.com>
2020-01-29 23:54:00 +0800

    DepartmentTopThreeSalaries

---
+++ src/code/code_151_200/code_184/DepartmentHighestSalary.java
@@ -0,0 +1,8 @@
+package code.code_151_200.code_184;
+
+public class DepartmentHighestSalary {
+
+    /*
+    * todo coding
+    * */
+}
---
+++ src/code/code_151_200/code_185/DepartmentTopThreeSalaries.java
@@ -0,0 +1,7 @@
+package code.code_151_200.code_185;
+
+public class DepartmentTopThreeSalaries {
+    /*
+     * todo coding
+     * */
+}
