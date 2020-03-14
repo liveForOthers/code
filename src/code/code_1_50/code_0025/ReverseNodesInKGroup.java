08383d0994f4f30bb0c44a6fcfc0e4512743a762
wangzuojun <wangzuojun@meituan.com>
2020-03-14 08:27:24 +0800

    test

--- src/code/code_1_50/code_0025/ReverseNodesInKGroup.java
+++ src/code/code_1_50/code_0025/ReverseNodesInKGroup.java
@@ -9,10 +9,8 @@ public class ReverseNodesInKGroup {
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(6);
-        head.next.next.next.next.next.next = new ListNode(7);
 
-        ListNode listNode = reverseKGroup(head, 3);
+        ListNode listNode = new ReverseNodesInKGroup().reverseKGroup2(head, 2);
         while (listNode != null) {
             System.out.println(listNode.val);
             listNode = listNode.next;
@@ -27,7 +25,7 @@ public class ReverseNodesInKGroup {
     *  2 cur 不会变化
     *  3 只有next 一直随着cur.next变化而变化
     * */
-    public static ListNode reverseKGroup(ListNode head, int k) {
+    public static ListNode reverseKGroup1(ListNode head, int k) {
         if (head == null) {
             return head;
         }
@@ -63,5 +61,48 @@ public class ReverseNodesInKGroup {
             prev = cur;
         }
         return dummyHead.next;
+    }
+
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        // 伪头结点
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        // 前继节点 头结点 尾巴节点  尾巴节点待初始化
+        ListNode prev = dummyHead;
+        ListNode curHead = head;
+        ListNode curTail = prev;
+        // 遍历列表
+        while (curHead != null) {
+            // 1 找当前段尾巴节点 不足k个 快速结束
+            int count = k;
+            while(curTail != null && count != 0) {
+                curTail = curTail.next;
+                count--;
+            }
+            if (curTail == null) {
+                break;
+            }
+            // 2 反转本段
+            reverse(prev, curHead, curTail);
+            // 3 更新 前继节点 头结点
+            prev = curHead;
+            curHead = prev.next;
+            curTail = prev;
+        }
+        // 返回伪头结点
+        return dummyHead.next;
+    }
+
+    private void reverse(ListNode prevNode, ListNode head, ListNode tail){
+        ListNode endNode = tail.next;
+        while (head.next != endNode) {
+            ListNode nextNode = head.next;
+            head.next = nextNode.next;
+            nextNode.next = prevNode.next;
+            prevNode.next = nextNode;
+        }
     }
 }
